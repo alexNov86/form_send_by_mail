4ce3b707289195a5cbaaa9e1978148d9546168c3
alexNovik <alexnovik86@gmail.com>
2022-09-12 23:24:16 +0300

    Third commit

--- js/script.js
+++ js/script.js
@@ -8,6 +8,28 @@ document.addEventListener("DOMContentLoaded", function () {
     event.preventDefault();
 
     let error = formValidate(form);
+    let formData = new FormData(form);
+    formData.append("image", formImage.files[0]);
+
+    if (error === 0) {
+      form.classList.add("_sending");
+      let response = await fetch("sendmail.php", {
+        method: "POST",
+        body: formData,
+      });
+      if (response.ok) {
+        let result = await response.json();
+        console.log(result.message);
+        formPreview.innerHTML = "";
+        form.reset();
+        form.classList.remove("_sending");
+      } else {
+        alert("Ошибка");
+        form.classList.remove("_sending");
+      }
+    } else {
+      alert("Заполните обязательные поля");
+    }
   }
 
   function formValidate(form) {
@@ -32,8 +54,8 @@ document.addEventListener("DOMContentLoaded", function () {
           error++;
         }
       }
-      console.log(error);
     }
+    return error;
   }
 
   function formAddError(input) {
@@ -46,5 +68,38 @@ document.addEventListener("DOMContentLoaded", function () {
   }
   function emailVerification(input) {
     return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+  }
+
+  const formImage = document.getElementById("formImage");
+  const formPreview = document.getElementById("formPreview");
+
+  formImage.addEventListener("change", () => {
+    fileVerification(formImage.files[0]);
+    uploadFile(formImage.files[0]);
+  });
+
+  function fileVerification(file) {
+    //проверка типа файла
+    if (!["image/jpeg", "image/png", "image/gif"].includes(file.type)) {
+      alert("Разрешены только изображения.");
+      formImage.value = "";
+      return;
+    }
+    //проверка размера файла (<2 Мб)
+    if (file.size > 2 * 1024 * 1024) {
+      alert("Файл должен быть менее 2 Мб.");
+      return;
+    }
+  }
+
+  function uploadFile(file) {
+    const reader = new FileReader();
+    reader.onload = function (event) {
+      formPreview.innerHTML = `<img src='${event.target.result}' alt='Фото'>`;
+    };
+    reader.onerror = function (event) {
+      alert("Ошибка загруженного изображения");
+    };
+    reader.readAsDataURL(file);
   }
 });
